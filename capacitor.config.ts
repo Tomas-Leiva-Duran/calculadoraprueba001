29625dea0eef6eb64a45c82c9ba56008f7077a41
Tomas-Leiva-Duran <to.leiva@duocuc.cl>
2024-01-12 00:11:14 -0300

    ok

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.calculadoraprueba001',
+  appName: 'calculadoraprueba001',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
